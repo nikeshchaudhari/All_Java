a7f4c441685942f0ddbc8fee87a1c5e335c14870
Nikesh Chaudhari <115630851+nikeshchaudhari@users.noreply.github.com>
2023-10-09 09:49:16 +0545

    Add files via upload
    Add Calc

---
+++ Calc.java
@@ -0,0 +1,19 @@
+public class Calc {
+    public static void main (String[] args){
+        int num1 = 10;
+        int num2 = 3;
+
+        int sum = num1+num2;
+        int diff = num1-num2;
+        int mul = num1*num2;
+        int div = num1/num2;
+        int mod = num1%num2;
+
+        System.out.println("Sum is "+ sum);
+        System.out.println("Diff is "+ diff);
+        System.out.println("Mul is "+ mul);
+        System.out.println("Div is "+div);
+        System.out.println("Mod is "+mod);
+    }
+}
+
